7f090ce9710016c48c6983f0913cc5de6e3fe206
TheMasterOfMagic <18811569079@163.com>
2020-05-02 22:40:59 +0800

    升级为优雅支持单进程多线程的脚本

--- model.py
+++ model.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker
 from utils import logging, f
 
 
 engine = create_engine('mysql://root@127.0.0.1:3306/test?charset=utf8')
 Session = sessionmaker(bind=engine)
+Session = scoped_session(Session)
 
 Base = declarative_base()
 
